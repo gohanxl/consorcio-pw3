a0dc887d8c5fb2571bb61bf442d1423ec40c9072
gohanxl <lucasmlacquaniti@gmail.com>
2020-10-16 20:28:03 -0300

    feat: add styles and Usuario entity

---
+++ Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Usuario
+    {
+        public Guid IdUsuario { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime FechaRegistracion { get; set; }
+        public DateTime UltimaFechaLogin { get; set; }
+    }
+}
