e79c0e2c84338fb5a08cf6b78033f35afcc79bd0
LUCAS MARIANO LACQUANITI <luclacquaniti@alumno.unlam.edu.ar>
2020-10-17 00:07:53 -0300

    feat: add mockup httpost

--- ConsorcioPW3/Controllers/HomeController.cs
+++ ConsorcioPW3/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,15 @@ namespace ConsorcioPW3.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CrearUsuario(FormCollection formCollection)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Email = formCollection["Email"];
+            usuario.Password = formCollection["Password"];
+
+            return View("Index");
         }
     }
 }
