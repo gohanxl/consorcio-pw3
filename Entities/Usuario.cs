c9fbe1b469490a291c5706f24e8861c1f78badd5
LUCAS MARIANO LACQUANITI <luclacquaniti@alumno.unlam.edu.ar>
2020-10-16 23:40:55 -0300

    feat: add notMapped validation for confirm password

--- Entities/Usuario.cs
+++ Entities/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities
 {
@@ -14,6 +15,10 @@ namespace Entities
         [Required(ErrorMessage = "El campo de Contraseña es obligatorio")]
         [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,64}$", ErrorMessage = "Te la comes")]
         public string Password { get; set; }
+
+        [NotMapped]
+        [Compare("Password", ErrorMessage = "Las contraseñas no son identicas, intente de nuevo.")]
+        public string ConfirmPassword { get; set; }
         public DateTime FechaRegistracion { get; set; }
         public DateTime UltimaFechaLogin { get; set; }
     }
