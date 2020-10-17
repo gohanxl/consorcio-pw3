682559a9a6d48c6386fcfddac88fd7228a080e6f
LUCAS MARIANO LACQUANITI <luclacquaniti@alumno.unlam.edu.ar>
2020-10-16 22:00:48 -0300

    feat: add validations with some styles

--- Entities/Usuario.cs
+++ Entities/Usuario.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
     public class Usuario
     {
         public Guid IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El campo de Email es obligatorio")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo de Contraseña es obligatorio")]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,64}$", ErrorMessage = "Te la comes")]
         public string Password { get; set; }
         public DateTime FechaRegistracion { get; set; }
         public DateTime UltimaFechaLogin { get; set; }
