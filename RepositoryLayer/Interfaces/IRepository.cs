7ac9b4860c083e59114d4eee7ae9752df8baee0b
LUCAS MARIANO LACQUANITI <luclacquaniti@alumno.unlam.edu.ar>
2020-10-22 23:10:08 -0300

    feat: create Repositories (EF) layer with an interface

---
+++ RepositoryLayer/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        //IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
