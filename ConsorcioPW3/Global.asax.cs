a6ad211e4ba41187a256d0f84d1c63301f089c90
Isa <isaias.caporusso@gmail.com>
2020-10-30 00:24:50 -0300

    Implements cookies to persist user information after login

--- ConsorcioPW3/Global.asax.cs
+++ ConsorcioPW3/Global.asax.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace ConsorcioPW3
 {
@@ -13,6 +15,30 @@ namespace ConsorcioPW3
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        public void Application_PostAuthenticateRequest()
+        {
+            HttpCookie cookie = Request.Cookies["SESSION"];
+
+            AuthenticationCookie(cookie);
+        }
+
+        public void AuthenticationCookie(HttpCookie cookie)
+        {
+            if (cookie != null)
+            {
+                String data = cookie.Value;
+                FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(data);
+
+                String email = ticket.Name;
+                String id = ticket.UserData;
+
+                GenericIdentity identity = new GenericIdentity(email);
+                GenericPrincipal user = new GenericPrincipal(identity, new String[] { id });
+
+                HttpContext.Current.User = user;
+            }
         }
     }
 }
