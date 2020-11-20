399f68b818a82ece87117b5c7d0b3c70aec3340b
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2020-11-20 16:27:17 -0300

    feat: add provincia service

---
+++ Services/Provincia/ProvinciaService.cs
@@ -0,0 +1,55 @@
+﻿using Repositories;
+using Repositories.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Provincia
+{
+    public class ProvinciaService<T> where T : class
+    {
+        ProvinciaRepository<T> provinciaRepository;
+
+        public ProvinciaService(ConsortiumContext context)
+        {
+            ConsortiumContext ctx = context;
+            provinciaRepository = new ProvinciaRepository<T>(ctx);
+        }
+
+        public void Delete(object id)
+        {
+            provinciaRepository.Delete(id);
+        }
+
+        public T GetById(object id)
+        {
+            return provinciaRepository.GetById(id);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            provinciaRepository.Insert(obj);
+        }
+
+        public void Save()
+        {
+            provinciaRepository.Save();
+        }
+
+        public void Update(T obj)
+        {
+            provinciaRepository.Update(obj);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return provinciaRepository.GetAll();
+        }
+    }
+}
