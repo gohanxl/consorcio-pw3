8b41dd63034f9fad8656bd4ebade577b62a83434
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2020-11-20 16:39:50 -0300

    feat: create expensa repository

---
+++ Repositories/Repositories/ExpensaRepository.cs
@@ -0,0 +1,87 @@
+﻿using Repositories.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace Repositories.Repositories
+{
+    public class ExpensaRepository<T> : IEnumerable<T>, IRepository<T> where T : class
+    {
+
+        ConsortiumContext expensasContext;
+        private DbSet<T> defaultObject;
+
+        public ExpensaRepository(ConsortiumContext context)
+        {
+            expensasContext = context;
+            defaultObject = expensasContext.Set<T>();
+        }
+
+        public void Delete(object id)
+        {
+            T existing = defaultObject.Find(id);
+            defaultObject.Remove(existing);
+            Save();
+        }
+
+        public T GetById(object id)
+        {
+            return defaultObject.Find(id);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            defaultObject.Add(obj);
+            Save();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return defaultObject.ToList();
+        }
+
+        public void Save()
+        {
+            try
+            {
+                expensasContext.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }
+
+        public void Update(T obj)
+        {
+            defaultObject.Add(obj);
+            expensasContext.Entry(obj).State = EntityState.Modified;
+            Save();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
